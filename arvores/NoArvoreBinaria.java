14e0685c556124db7e57af2dafd7e07016831996
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-04-19 21:57:12 -0300

    Arvore Binaria

---
+++ arvores/NoArvoreBinaria.java
@@ -0,0 +1,82 @@
+package arvores;
+import java.security.InvalidParameterException;
+public class NoArvoreBinaria<T>
+{
+    private T info;
+    private NoArvoreBinaria<T> esq;
+    private NoArvoreBinaria<T> dir;
+    
+    public NoArvoreBinaria(T info)
+    {
+        this.setInfo(info);
+    }
+    public NoArvoreBinaria(T info, NoArvoreBinaria<T> esq, NoArvoreBinaria<T> dir)
+    {
+        this(info);
+        this.setEsq(esq);
+        this.setDir(dir);
+    }
+    
+    public T getInfo()
+    {
+        return info;
+    }
+    private void setInfo(T info)
+    {
+        if (info == null)
+            throw new InvalidParameterException("Info não pode ser nulo");
+        this.info = info;
+    }
+    public NoArvoreBinaria<T> getEsq()
+    {
+        return esq;
+    }
+    private void setEsq(NoArvoreBinaria<T> esq)
+    {
+        this.esq = esq;
+    }
+    public NoArvoreBinaria<T> getDir()
+    {
+        return dir;
+    }
+    private void setDir(NoArvoreBinaria<T> dir)
+    {
+        this.dir = dir;
+    }
+    
+    public NoArvoreBinaria<T> pertenceDoNo(T info)
+    {
+        if (this.getInfo().equals(info))
+            return this;
+        
+        if (this.esq != null)
+        {
+            NoArvoreBinaria<T> noEsq = this.esq.pertenceDoNo(info);
+            if (noEsq != null)
+                return noEsq;
+        }
+        if (this.dir != null)
+        {
+            NoArvoreBinaria<T> noDir = this.dir.pertenceDoNo(info);
+            if (noDir != null)
+                return noDir;
+        }
+        return null;
+    }
+    
+    public String imprimePre()
+    {
+        String retorno = this.getInfo() + "<";
+        if (this.getEsq() != null)
+            retorno += this.getEsq().imprimePre();
+        retorno += "><";
+        if (this.getDir() != null)
+            retorno += this.getDir().imprimePre();
+        return retorno + ">";
+    }
+    
+    public String toString()
+    {
+        return this.getInfo().toString();
+    }
+}
---
+++ arvores/TesteArvoreBinaria.java
@@ -0,0 +1,35 @@
+package arvores;
+
+public class TesteArvoreBinaria {
+
+	public static void main(String[] args) {
+
+		NoArvoreBinaria<Integer> no4 = new NoArvoreBinaria<>(4);
+		NoArvoreBinaria<Integer> no5 = new NoArvoreBinaria<>(5);
+		NoArvoreBinaria<Integer> no2 = new NoArvoreBinaria<>(2, no4, no5);
+
+		NoArvoreBinaria<Integer> no7 = new NoArvoreBinaria<>(7);
+		NoArvoreBinaria<Integer> no6 = new NoArvoreBinaria<>(6, no7, null);
+
+		NoArvoreBinaria<Integer> no3 = new NoArvoreBinaria<>(3, null, no6);
+
+		NoArvoreBinaria<Integer> no1 = new NoArvoreBinaria<>(1, no2, no3);
+
+		ArvoreBinaria<Integer> arvore = new ArvoreBinaria<>();
+		arvore.setRaiz(no1);
+
+		//System.out.println(no5.toString());
+
+		NoArvoreBinaria<Integer> teste;
+
+		teste = arvore.pertence(66);
+		System.out.println("Null-->"+teste);
+		
+		teste = arvore.pertence(6);
+		System.out.println("6-->"+teste);
+		
+		System.out.println(arvore);
+
+	}
+
+}
